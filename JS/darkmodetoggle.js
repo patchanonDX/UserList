321e5e5edcfd7ada3508cd734f33647ade4f85df
Patchanon Suwanlertcharoen <patchanon.suw@dome.tu.ac.th>
2026-09-25 23:25:09 +0700

    Complete the rest2

--- JS/darkmodetoggle.js
+++ JS/darkmodetoggle.js
@@ -1,8 +1,4 @@
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2025 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
+
 
 (() => {
   'use strict'
